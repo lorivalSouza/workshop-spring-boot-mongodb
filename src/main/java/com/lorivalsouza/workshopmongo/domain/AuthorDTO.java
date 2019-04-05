9863836dbd8b7b254088f80d54f5eca2894c414c
Lorival Souza <lorivalnew@gmail.com>
2019-04-05 15:26:48 -0300

    Progjecao dados autor co DTO

---
+++ src/main/java/com/lorivalsouza/workshopmongo/domain/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.lorivalsouza.workshopmongo.domain;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	
+//	@DBRef(lazy = true)
+//	private List<Post> posts = new ArrayList<>();
+	
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/main/java/com/lorivalsouza/workshopmongo/domain/Post.java
+++ src/main/java/com/lorivalsouza/workshopmongo/domain/Post.java
@@ -15,14 +15,14 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 	}
 
 	
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -81,13 +81,13 @@ public class Post implements Serializable {
 
 
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
