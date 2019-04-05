2e40d8ad1770c8195cd987b0c8b2993b754fe7cf
Lorival Souza <lorivalnew@gmail.com>
2019-04-05 15:52:02 -0300

    Endpoit para retornar os posts do usuario

--- src/main/java/com/lorivalsouza/workshopmongo/domain/Post.java
+++ src/main/java/com/lorivalsouza/workshopmongo/domain/Post.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
@@ -17,10 +17,10 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	//private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
-
-	
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
@@ -30,74 +30,54 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
-	
-	
 
 	public String getId() {
 		return id;
 	}
 
-
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
-
 
 	public Date getDate() {
 		return date;
 	}
 
-
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
-
 
 	public String getTitle() {
 		return title;
 	}
 
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
 
 	public String getBody() {
 		return body;
 	}
 
-
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-
-
 
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-
-
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-
+//	public List<CommentDTO> getComments() {
+//		return comments;
+//	}
+//
+//	public void setComments(List<CommentDTO> comments) {
+//		this.comments = comments;
+//	}
 
 	@Override
 	public int hashCode() {
@@ -123,5 +103,4 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
--- src/main/java/com/lorivalsouza/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lorivalsouza/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lorivalsouza.workshopmongo.domain.Post;
 import com.lorivalsouza.workshopmongo.domain.User;
 import com.lorivalsouza.workshopmongo.dto.UserDTO;
 import com.lorivalsouza.workshopmongo.services.UserService;
@@ -65,6 +66,11 @@ public class UserResource {
 
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 
 }
